0ad16909b82957cd0b5e593849391d6d63e17f93
Oskar Karpiński <me@medzik.dev>
2024-09-11 21:06:00 +0200

    Extend material ColorScheme
    Added "warning", "info" and "ok" colors (content color, container and on container).
    Also added "Normal", "Subtitle" and "DisabledComponent" alpha values.

--- lib/src/commonMain/kotlin/dev/medzik/jetkit/theme/Color.kt
+++ lib/src/commonMain/kotlin/dev/medzik/jetkit/theme/Color.kt
@@ -1,5 +1,157 @@
 package dev.medzik.jetkit.theme
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.graphics.luminance
 
+object Alpha {
+    const val Normal = 1f
+    const val Subtitle = 0.6f
+    const val DisabledComponent = 0.38f
+}
+
+/**
+ * Returns a new [Color] with alpha multiplied by given [alpha] value.
+ *
+ * @param alpha the alpha value to multiple by.
+ * @return A new [Color] with alpha multiplied by given [alpha] value.
+ */
 fun Color.combineAlpha(alpha: Float): Color = copy(alpha = this.alpha * alpha)
+
+/**
+ * Returns `true` if the background color is dark, `false` otherwise.
+ */
+val ColorScheme.isDark
+    get() = background.luminance() < 0.5f
+
+private object Hue {
+    const val WARNING = 55f
+    const val INFO = 210f
+    const val OK = 120f
+}
+
+val ColorScheme.warning: Color
+    @Composable
+    get() = buildContentColor(Hue.WARNING)
+
+val ColorScheme.info: Color
+    @Composable
+    get() = buildContentColor(Hue.INFO)
+
+val ColorScheme.ok: Color
+    @Composable
+    get() = buildContentColor(Hue.OK)
+
+@Composable
+private fun ColorScheme.buildContentColor(
+    hue: Float,
+): Color {
+    val isDark = isDark
+    val color = remember(isDark) {
+        buildContentColor(
+            hue = hue,
+            onDark = isDark,
+        )
+    }
+    return color
+}
+
+private fun buildContentColor(
+    hue: Float,
+    onDark: Boolean,
+): Color {
+    val saturation: Float
+    val lightness: Float
+    if (onDark) {
+        saturation = 0.72f // saturation
+        lightness = 0.86f // value
+    } else {
+        saturation = 0.96f // saturation
+        lightness = 0.80f // value
+    }
+    return Color.hsv(
+        hue = hue,
+        saturation = saturation,
+        value = lightness,
+    )
+}
+
+val ColorScheme.warningContainer: Color
+    @Composable
+    get() = buildContainerColor(Hue.WARNING)
+
+val ColorScheme.onWarningContainer: Color
+    @Composable
+    get() = buildOnContainerColor(Hue.WARNING)
+
+val ColorScheme.infoContainer: Color
+    @Composable
+    get() = buildContainerColor(Hue.INFO)
+
+val ColorScheme.onInfoContainer: Color
+    @Composable
+    get() = buildOnContainerColor(Hue.INFO)
+
+val ColorScheme.okContainer: Color
+    @Composable
+    get() = buildContainerColor(Hue.OK)
+
+val ColorScheme.onOkContainer: Color
+    @Composable
+    get() = buildOnContainerColor(Hue.OK)
+
+@Composable
+private fun ColorScheme.buildContainerColor(
+    hue: Float,
+): Color {
+    val tint = Color.hsv(
+        hue = hue,
+        saturation = 1f,
+        value = 1f,
+    )
+    val backgroundColor = if (isDark) {
+        surfaceVariant
+    } else {
+        surfaceVariant
+    }
+    return tint
+        .copy(alpha = 0.20f)
+        .compositeOver(backgroundColor)
+}
+
+@Composable
+private fun ColorScheme.buildOnContainerColor(
+    hue: Float,
+): Color {
+    val isDark = isDark
+    val color = remember(isDark) {
+        buildOnContainerColor(
+            hue = hue,
+            onDark = isDark,
+        )
+    }
+    return color
+}
+
+private fun buildOnContainerColor(
+    hue: Float,
+    onDark: Boolean,
+): Color {
+    val saturation: Float
+    val lightness: Float
+    if (onDark) {
+        saturation = 0.1f // saturation
+        lightness = 0.9f // value
+    } else {
+        saturation = 0.2f // saturation
+        lightness = 0.2f // value
+    }
+    return Color.hsv(
+        hue = hue,
+        saturation = saturation,
+        value = lightness,
+    )
+}
--- sample/shared/src/commonMain/kotlin/App.kt
+++ sample/shared/src/commonMain/kotlin/App.kt
@@ -1,19 +1,138 @@
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import dev.medzik.jetkit.theme.Alpha
+import dev.medzik.jetkit.theme.combineAlpha
+import dev.medzik.jetkit.theme.info
+import dev.medzik.jetkit.theme.infoContainer
+import dev.medzik.jetkit.theme.ok
+import dev.medzik.jetkit.theme.okContainer
+import dev.medzik.jetkit.theme.onInfoContainer
+import dev.medzik.jetkit.theme.onOkContainer
+import dev.medzik.jetkit.theme.onWarningContainer
+import dev.medzik.jetkit.theme.warning
+import dev.medzik.jetkit.theme.warningContainer
 
 @Composable
 fun App() {
     MaterialTheme {
         Column(
             modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Text("Test")
+            Text(
+                text = "Alpha",
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Surface(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier.width(120.dp)
+                ) {
+                    Text(
+                        text = "Normal",
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier.padding(12.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                Surface(
+                    color = MaterialTheme.colorScheme.primary.combineAlpha(Alpha.DisabledComponent),
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier.width(120.dp)
+                ) {
+                    Text(
+                        text = "Disabled",
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier.padding(12.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            Text(
+                text = "Colors",
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Surface(
+                    color = MaterialTheme.colorScheme.warningContainer,
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier.width(120.dp)
+                ) {
+                    Text(
+                        text = "Warning",
+                        color = MaterialTheme.colorScheme.onWarningContainer,
+                        modifier = Modifier.padding(12.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                Surface(
+                    color = MaterialTheme.colorScheme.infoContainer,
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier.width(120.dp)
+                ) {
+                    Text(
+                        text = "Info",
+                        color = MaterialTheme.colorScheme.onInfoContainer,
+                        modifier = Modifier.padding(12.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                Surface(
+                    color = MaterialTheme.colorScheme.okContainer,
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier.width(120.dp)
+                ) {
+                    Text(
+                        text = "Ok",
+                        color = MaterialTheme.colorScheme.onOkContainer,
+                        modifier = Modifier.padding(12.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Text(
+                    text = "Warning",
+                    color = MaterialTheme.colorScheme.warning
+                )
+
+                Text(
+                    text = "Info",
+                    color = MaterialTheme.colorScheme.info
+                )
+
+                Text(
+                    text = "Ok",
+                    color = MaterialTheme.colorScheme.ok
+                )
+            }
         }
     }
 }
